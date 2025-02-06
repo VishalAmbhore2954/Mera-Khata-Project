beae21ec60a2b5a62d85fccb553d026240935272
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-02-06 22:01:07 +0530

    Made files for Add given items to customers php  file

---
+++ AddGiven.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="DisplayCustomer.css">
+
+<?php
+    error_reporting(0);
+    include("connection.php");
+    $sql = "SELECT * FROM customer";
+    $result = mysqli_query($con,$sql);
+    // $row = mysqli_fetch_assoc($result);
+
+    ?>
+    <a href="Home.php" id="home">GO HOME</a>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Mobile</th>
+            <th>Email</th>
+            <th>Remain Amount</th>
+            <th>Operations</th>
+        </tr>
+    
+    <?php
+
+    while($row =mysqli_fetch_assoc($result)){
+        $sum = $sum+$row["total"];
+
+        echo "<tr>
+        <td>".$row["id"]."</td>
+        <td>".$row["name"]."</td>
+        <td>".$row["mobile"]."</td>
+        <td>".$row["email"]."</td>
+        <td>".$row["total"]."</td>
+        <td><a href='given.php?mno=$row[mobile]' id='given'>Add Given</a><a href='' id='detail'>Detail</a></td>        
+        </tr>";
+    }
+    ?>
+        </table>
+        <div class='sum'>
+            <label for="">Total Amount To Credit : </label>
+            &#8377; <?php echo $sum; ?> 
+        </div>
+    <?php
+?>
