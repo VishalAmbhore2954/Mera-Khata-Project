e30e93d96e3d0e4e089103b3ebcd1f31daf106fd
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-02-06 18:36:10 +0530

    Seperate some code from home file and made a new file for that code as SearchData.php and include it into home.php file

--- Home.php
+++ Home.php
@@ -1,22 +1,6 @@
 <?php
-    include('connection.php');
-
-    if(isset($_POST['submit'])){
-        $search = $_POST['search'];
-
-        $query = "SELECT * FROM customer WHERE name = '$search' or mobile = '$search'";
-        $result = mysqli_query($con,$query);
-        $data = mysqli_num_rows($result);
-        // echo "<script>alert($data)</script>";
-        if($data>0){
-            header('location:given.php');
-        }else{
-            // echo "<script>alert('No Record Found!')</scrip";
-            header('location:notFound.php');
-        }
-    }
+    include("SearchData.php");
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
