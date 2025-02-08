4a7193160cc70c404aa6416c86bee639e7f0e9f8
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-02-08 12:06:43 +0530

    Created congrats.php

---
+++ congrats.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: #86d2c5;
+            display: flex;
+            justify-content: center;
+
+        }
+        .container {
+            margin-top: 50px;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        img {
+            width: 200px;
+            margin-left: 37px;
+            display: inline;
+        }
+        label {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            font-weight: bolder;
+            font-size: 30px;
+            color: red;
+        }
+
+        #con {
+            color :green;
+        }
+    </style>
+</head>
+<body>
+<!-- <img src="backArrow" alt="Go Home" onclick="Home.php"> -->
+    <div class="container">
+        <img id="" src="HappyImg.png" alt="fun imogy"><br>
+        <label for="" id="con">PAYMENT RECIEVED!!</label><br>
+        <a href="Home.php">GO HOME</a>
+    </div>
+</body>
+</html>
