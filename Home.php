9e451c716b00582be3160bb0dabe5ee8223a4558
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-02-08 20:59:50 +0530

    apply internal style to add customer button and formated code

--- Home.php
+++ Home.php
@@ -1,15 +1,28 @@
 <?php
-    include("SearchData.php");
+include("SearchData.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mera Khata</title>
     <link rel="stylesheet" href="Home.css">
+    <style>
+        #cust {
+            display: inline;
+            border: 1px solid black;
+            padding: 5px;
+            font-size: 12px;
+            border-radius: 5px;
+            background-color: #FDF89F;
+            color: #272501;
+        }
+    </style>
 </head>
+
 <body>
     <div class="nav">
         <div>
@@ -17,9 +30,10 @@
         </div>
         <span id="logoname">मेरा खाता</span>
         <form method="POST">
-            <div class="search"><input type="text" name="search" placeholder="  Enter Mobile Number / Customer name" required><button name="submit">Search</button></div>
+            <div class="search"><input type="text" name="search" placeholder="  Enter Mobile Number / Customer name"
+                    required><button name="submit">Search</button></div>
         </form>
-        <a href="addCustomer.php">+Add Customer</a>
+        <a href="addCustomer.php" type="button" id="cust">&#128104;Add Customer</a>
     </div>
     <div class="bodyImage">
         <img src="Peacock-Clipart-PNG-1.png" alt="">
@@ -27,7 +41,9 @@
     <div class="btns">
         <a type="button" id="add" href="addGiven.php">Add Given</a>
         <a type="button" id="credit" href="addCredit.php">Add Credit</a>
-        <a type="button" id="allCust" href="DisplayCustomer.php" style="background-color:blue; color:white">Customers</a>
+        <a type="button" id="allCust" href="DisplayCustomer.php"
+            style="background-color:blue; color:white">Customers</a>
     </div>
 </body>
+
 </html>
