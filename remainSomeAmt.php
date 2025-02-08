7fcc71b0c50d6e6a58fde3701819d55525bce7be
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-02-08 19:48:13 +0530

    create page for remain some amount after credit

---
+++ remainSomeAmt.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: #86d2c5;
+            display: flex;
+            justify-content: center;
+
+        }
+        .container {
+            margin-top: 50px;
+           text-align: center;
+        }
+
+        a {
+            text-decoration: none;
+            display: inline;
+            border: 1px solid black;
+            padding: 5px;
+            margin: 10px;
+            border-radius: 5px;
+            background-color: #FDF89F;
+            color: black;
+        }
+
+        img {
+            width: 200px;
+            display: inline;
+        }
+
+        img:hover {
+            width: 205px;
+            display: inline;
+        }
+
+        label {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            font-weight: bolder;
+            font-size: 20px;
+            color: red;
+        }
+
+        #con {
+            color : red;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img id="" src="shocked.png" alt="fun imogy"><br>
+        <label for="" id="con">STILL SOME AMOUNT REMAIND TO CREDIT!!</label><br>
+        <label for="" id="con">TRY TO CLEAR ACCOUNT AS SOON AS!!</label><br><br>
+        <a href="Home.php">GO HOME</a>
+    </div>
+</body>
+</html>
